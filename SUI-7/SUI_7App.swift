b7b0473f10fc3b92bb3b577db760252b739c7ee4
TURKCELL/Pavel Nepogodin <pavel.nepogodin@consultant.turcell.com.tr>
2023-10-17 14:56:52 +0300

    Initial Commit

---
+++ SUI-7/SUI_7App.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_7App.swift
+//  SUI-7
+//
+//  Created by  Pavel Nepogodin on 17.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SUI_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
